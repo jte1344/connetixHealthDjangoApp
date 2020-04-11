d7f9b7e38aa8555c7404b52f5c7a1e3cbb4462e5
SeamusDowney <42756562+SeamusDowney@users.noreply.github.com>
2020-04-10 22:27:36 -0600

    Add files via upload

--- NihMedicationInteraction.py
+++ NihMedicationInteraction.py
@@ -15,6 +15,7 @@ def grab_rxcui(medication_name):
     medication_string = "name=" + medication_name + "&search=1"
     rxcui_response = requests.get(api_url_base + api_rxcui, medication_string)
     rxcui_ouput = rxcui_response.json()
+
 #   print(json.dumps(rxcui_ouput, indent=4, sort_keys=True))                                                 DEBUG LINE
     return rxcui_ouput
 
@@ -24,51 +25,44 @@ def grab_interactions(rxcuis):
     rxcui_param = "rxcuis=" + rxcuis + "&sources=DrugBank"
     interaction_response = requests.get(api_url_base + api_interaction, rxcui_param)
     interaction_output = interaction_response.json()
+    
 #   print(json.dumps(interaction_output, indent=4, sort_keys=True))                                          DEBUG LINE
     return interaction_output
 
-# prompts user to input list of medication names
-# appends all medication names into medication_list
-console = 1
-medication_list = []
-while console == 1:
-    user_input = input()
-    if user_input == "end":
-        console = 0
+def call_interactions(medication_list):
+    medication_list = medication_list.split(", ")
+    warning = []
 
-    if console != 0:
-        medication_list.append(user_input)
+    # calls grab_rxcui to get medications rxcui ids
+    # places json outputs into rxcui_list
+    rxcui_list = []
+    for medication in medication_list:
+        rxcui_list.append(grab_rxcui(medication))
 
-# calls grab_rxcui to get medications rxcui ids
-# places json outputs into rxcui_list
-rxcui_list = []
-for medication in medication_list:
-    rxcui_list.append(grab_rxcui(medication))
-
-# appends all rxcuis into a string for grab_interactions parameter
-rxcui_string = ""
-for rxcui in rxcui_list:
-    if "rxnormId" in rxcui['idGroup']:
-#       print(rxcui['idGroup']['rxnormId'][0])                                                               DEBUG LINE
-        if rxcui != rxcui_list[-1] and rxcui_list.__len__() > 1:
-            rxcui_string = rxcui_string + rxcui['idGroup']['rxnormId'][0] + "+"
+    # appends all rxcuis into a string for grab_interactions parameter
+    rxcui_string = ""
+    for rxcui in rxcui_list:
+        if "rxnormId" in rxcui['idGroup']:
+#           print(rxcui['idGroup']['rxnormId'][0])                                                           DEBUG LINE
+            if rxcui != rxcui_list[-1] and rxcui_list.__len__() > 1:
+                rxcui_string = rxcui_string + rxcui['idGroup']['rxnormId'][0] + "+"
+            else:
+                rxcui_string = rxcui_string + rxcui['idGroup']['rxnormId'][0]
         else:
-            rxcui_string = rxcui_string + rxcui['idGroup']['rxnormId'][0]
-    else:
-        print("\nInvalid medication name entered...")
+            warning.append("Invalid medication name entered...")
 
-# grabs drug interactions of given medications
-drug_interactions = grab_interactions(rxcui_string)
+    # grabs drug interactions of given medications
+    drug_interactions = grab_interactions(rxcui_string)
 
-# print resulting interactions between drugs to console
-if rxcui_list.__len__() > 1:
-    if "fullInteractionTypeGroup" in drug_interactions:
-        print("\nPotential interactions between the listed medications:")
-        for interaction in drug_interactions['fullInteractionTypeGroup'][0]['fullInteractionType']:
-            print(interaction['interactionPair'][0]["description"])
-    else:
-        print("\nNo interactions found between the listed medications.")
+    # print resulting interactions between drugs to console
+    interaction_list = []
+    if rxcui_list.__len__() > 1:
+        if "fullInteractionTypeGroup" in drug_interactions:
+            warning.append("Potential interactions between the listed medications:")
+            for interaction in drug_interactions['fullInteractionTypeGroup'][0]['fullInteractionType']:
+                interaction_list.append(interaction['interactionPair'][0]["description"])
+        else:
+            warning.append("No interactions found between the listed medications.")
+    data = {"User Input:": medication_list, "Warning": warning, "Interactions": interaction_list}
 
-# TO DO:
-#       change from user input of medications to read input from website (Lines 32-40)
-#       change output from printing to console to print to website (Lines 58, 66, 68, 70)
+    return data
