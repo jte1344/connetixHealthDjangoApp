6ba0a7393ff2dbcad191f7d348033eadb9c4659f
Jake Engelen <jte1544@yahoo.com>
2020-04-25 16:35:18 -0600

    Init push

--- index/static/index/angular/medAngular.js
+++ index/static/index/angular/medAngular.js
@@ -126,49 +126,53 @@ medApp.controller('homeCtrl', function($scope, $http) {
 medApp.controller('mapsCtrl', function($scope, $window) {
 
 
-  $scope.google = ""
   $scope.lat = "38.893137"
   $scope.lng = "-104.800630"
 
-  var mapOptions = {
-    zoom: 11,
-    center: new google.maps.LatLng($scope.lat, $scope.lng),
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-  }
+  if (google == null) {
+    console.log("var google is undefined")
+  } else {
 
-  if (document.getElementById('map')) {
-    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  }
+    var mapOptions = {
+      zoom: 11,
+      center: new google.maps.LatLng($scope.lat, $scope.lng),
+      mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
 
-  $scope.markers = [];
+    if (document.getElementById('map')) {
+      $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    }
 
-  var infoWindow = new google.maps.InfoWindow();
+    $scope.markers = [];
 
-  var createMarker = function(info) {
+    var infoWindow = new google.maps.InfoWindow();
 
-    var marker = new google.maps.Marker({
-      map: $scope.map,
-      position: new google.maps.LatLng(info.lat, info.long),
-      title: info.city
-    });
-    marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+    var createMarker = function(info) {
 
-    google.maps.event.addListener(marker, 'click', function() {
-      infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-      infoWindow.open($scope.map, marker);
-    });
+      var marker = new google.maps.Marker({
+        map: $scope.map,
+        position: new google.maps.LatLng(info.lat, info.long),
+        title: info.city
+      });
+      marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
 
-    $scope.markers.push(marker);
-  }
+      google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+        infoWindow.open($scope.map, marker);
+      });
+
+      $scope.markers.push(marker);
+    }
 
 
-  for (i = 0; i < cities.length; i++) {
-    createMarker(cities[i]);
-  }
+    for (i = 0; i < cities.length; i++) {
+      createMarker(cities[i]);
+    }
 
-  $scope.openInfoWindow = function(e, selectedMarker) {
-    e.preventDefault();
-    google.maps.event.trigger(selectedMarker, 'click');
+    $scope.openInfoWindow = function(e, selectedMarker) {
+      e.preventDefault();
+      google.maps.event.trigger(selectedMarker, 'click');
+    }
   }
 
 })
